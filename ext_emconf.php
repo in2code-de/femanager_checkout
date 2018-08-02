2bb3fb4867e9a66228f74482ee1940082771bfb0
Stefan Busemann <stefan.busemann@in2code.de>
2018-08-02 15:33:37 +0200

    [TASK] Add composer info

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,7 +7,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'femanager checkout',
     'description' => 'Enables a checkout process for frontend users. During the registration users are redirected to a 
-    ',
+    paypal checkout page',
     'category' => 'plugin',
     'author' => 'Stefan Busemann',
     'author_email' => 'info@in2code.de',
@@ -15,14 +15,14 @@ $EM_CONF[$_EXTKEY] = [
     'shy' => '',
     'priority' => '',
     'module' => '',
-    'state' => 'beta',
+    'state' => 'stable',
     'internal' => '',
     'uploadfolder' => '0',
     'createDirs' => '',
     'modify_tables' => '',
     'clearCacheOnLoad' => 0,
     'lockType' => '',
-    'version' => '0.9.0',
+    'version' => '1.0.0',
     'constraints' => [
         'depends' => [
             'typo3' => '8.7.0-8.7.99',
