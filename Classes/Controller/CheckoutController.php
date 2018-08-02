7ae59ab96f06ee26a2a146d7c166a015860540ea
Stefan Busemann <stefan.busemann@in2code.de>
2018-08-02 16:53:37 +0200

    [FEATURE] Add configuration check

--- Classes/Controller/CheckoutController.php
+++ Classes/Controller/CheckoutController.php
@@ -8,7 +8,6 @@ use In2code\Femanager\Domain\Model\User;
 use TYPO3\CMS\Core\Messaging\AbstractMessage;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
-use TYPO3\CMS\Extbase\Mvc\Controller\Arguments;
 
 /**
  * Class NewController
@@ -45,8 +44,6 @@ class CheckoutController extends ActionController
      */
     public function checkoutAction()
     {
-        \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($this->settings, __FILE__ . '@' . __LINE__);
-
         $feUserUid = $GLOBALS['TSFE']->fe_user->user['uid'];
         if (is_null($feUserUid)) {
             $this->addFlashMessage('You have to be logged in', 'Login nessecary', AbstractMessage::ERROR);
@@ -55,6 +52,32 @@ class CheckoutController extends ActionController
         $feUser = $this->userRepository->findByUid($feUserUid);
         $this->view->assign('FeUser', $feUser);
 
+    }
+
+    /**
+     * @throws \TYPO3\CMS\Extbase\Mvc\Exception\StopActionException
+     * @throws \TYPO3\CMS\Extbase\Mvc\Exception\UnsupportedRequestTypeException
+     */
+    public function initializeCheckoutAction()
+    {
+        $this->controllerContext = $this->buildControllerContext();
+
+        // check settings
+        if (!$this->settings['groups']['basis'] OR !$this->settings['groups']['premium'] )
+        {
+            $this->addFlashMessage('TypoScript setting for user groups is missing', 'Configuration Error', AbstractMessage::ERROR);
+            $this->redirect('message');
+        }
+        if (!$this->settings['paypal']['client']['sandbox'] AND !$this->settings['paypal']['client']['production'] )
+        {
+            $this->addFlashMessage('TypoScript setting for paypal environment is missing', 'Configuration Error', AbstractMessage::ERROR);
+            $this->redirect('message');
+        }
+        if (!$this->settings['paypal']['client']['payment']['transactions']['amount']['total'] || !$this->settings['paypal']['client']['payment']['transactions']['amount']['currency'] )
+        {
+            $this->addFlashMessage('TypoScript setting for paypal payment is missing', 'Configuration Error', AbstractMessage::ERROR);
+            $this->redirect('message');
+        }
     }
 
     /**
@@ -76,11 +99,10 @@ class CheckoutController extends ActionController
             $this->addFlashMessage('Something went wrong with your payment', 'Error', AbstractMessage::ERROR);
             $this->redirect('message');
         }
-        $basisGroup = $this->userGroupRepository->findByUid($this->settings['groups']['premium']);
+        $basisGroup = $this->userGroupRepository->findByUid($this->settings['groups']['basis']);
         $premiumGroup = $this->userGroupRepository->findByUid($this->settings['groups']['premium']);
-        $user->addUsergroup($premiumGroup);
         $user->removeUsergroup($basisGroup);
-        $this->userRepository->update($user);
+        $user->addUsergroup($premiumGroup);
     }
 
     /**
@@ -108,9 +130,31 @@ class CheckoutController extends ActionController
      */
     private function verifyPayment($arguments)
     {
-        If ($arguments['token'] && $arguments['paymentId'] && $arguments['PayerID']){
+        If ($arguments['token'] && $arguments['paymentId'] && $arguments['PayerID']) {
             return true;
         }
+
         return false;
     }
+    /**
+     * Initialize the controller context
+     *
+     * @return \TYPO3\CMS\Extbase\Mvc\Controller\ControllerContext ControllerContext to be passed to the view
+     * @api
+     */
+    protected function buildControllerContext()
+    {
+        /** @var $controllerContext \TYPO3\CMS\Extbase\Mvc\Controller\ControllerContext */
+        $controllerContext = $this->objectManager->get(\TYPO3\CMS\Extbase\Mvc\Controller\ControllerContext::class);
+        $controllerContext->setRequest($this->request);
+        $controllerContext->setResponse($this->response);
+        if ($this->arguments !== null) {
+            $controllerContext->setArguments($this->arguments);
+        }
+        $controllerContext->setUriBuilder($this->uriBuilder);
+
+        return $controllerContext;
+    }
 }
+
+
